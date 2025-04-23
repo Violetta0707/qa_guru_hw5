add9a389d614e546581e4fa13e1513c25829a044
Violetta Novikova <9006524258@mail.ru>
2025-04-23 04:30:14 +0300

    добавила аватарку, провела тест

---
+++ hw5_test_tools.qa.py
@@ -0,0 +1,41 @@
+from selene import browser, have, be, command
+
+
+def test_fill_practice_form():
+    browser.open('/automation-practice-form')
+
+    browser.element('#firstName').type('Новикова')
+    browser.element('#lastName').type('Виолетта')
+    browser.element('#userEmail').type('violet@example.com')
+
+    browser.element('[for="gender-radio-1"]').click()
+
+    browser.element('#userNumber').type('1234567890')
+
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').type('July')
+    browser.element('.react-datepicker__year-select').type('2002')
+    browser.element('.react-datepicker__day--001').click()
+
+    browser.element('#subjectsInput').type('Maths').press_enter()
+
+    browser.element('[for="hobbies-checkbox-1"]').click()
+
+    browser.element('#uploadPicture').set_value(
+        str(__file__).replace('test_practice_form.py', 'resources/avatar.png')
+    )
+
+    browser.element('#currentAddress').type('ул. Садовая, д. 1')
+
+    browser.element('#state').click()
+    browser.element('#react-select-3-option-0').click()
+    browser.element('#city').click()
+    browser.element('#react-select-4-option-0').click()
+
+    browser.element('#submit').click()
+
+    browser.element('.modal-header').should(have.text('Thanks for submitting the form'))
+    browser.element('.table').should(have.text('Виолетта Новикова'))
+    browser.element('.table').should(have.text('violet@example.com'))
+    browser.element('.table').should(have.text('Female'))
+    browser.element('.table').should(have.text('1234567890'))
